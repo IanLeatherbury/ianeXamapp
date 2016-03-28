78ed09d936499c34c0d64c955095e1f437368ee2
Ian Leatherbury <ian.leatherbury@gmail.com>
2016-03-28 13:56:03 -0700

    finished step 3, also added tfs version control for kicks

--- ianexamapp/HomePage.cs
+++ ianexamapp/HomePage.cs
@@ -4,18 +4,22 @@ using Xamarin.Forms;
 
 namespace ianexamapp
 {
-	public class HomePage : ContentPage
-	{
-		public HomePage ()
-		{
-			var abs = new AbsoluteLayout ();
-			var button = new Button{Text = "Start eXam!", TextColor = Color.Navy, BackgroundColor = Color.Silver };
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            //Set background image
+            var backgroundImage = new Image { Source = ImageSource.FromResource("ianexamapp.Images.background.jpg") };
+            backgroundImage.Aspect = Aspect.AspectFill;
 
-			abs.Children.Add (button, new Rectangle (0.5, 0.5, 150, 60), AbsoluteLayoutFlags.PositionProportional);
+            var abs = new AbsoluteLayout();
+            var button = new Button { Text = "Start eXam!", TextColor = Color.Navy, BackgroundColor = Color.Silver };
 
-			Content = abs;
-		}
-	}
+            //add background image first, so it's first in the z order
+            abs.Children.Add(backgroundImage, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.SizeProportional);
+            abs.Children.Add(button, new Rectangle(0.5, 0.5, 150, 60), AbsoluteLayoutFlags.PositionProportional);           
+
+            Content = abs;
+        }
+    }
 }
-
-
