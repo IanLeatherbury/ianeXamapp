ffc947d2d76a06d9a9c86e094adf447758cce500
Ian Leatherbury <ian.leatherbury@gmail.com>
2016-03-28 08:23:42 -0700

    initial commit

---
+++ ianexamapp/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ianexamapp
+{
+	public class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			var abs = new AbsoluteLayout ();
+			var button = new Button{Text = "Start eXam!", TextColor = Color.Navy, BackgroundColor = Color.Silver };
+
+			abs.Children.Add (button, new Rectangle (0.5, 0.5, 150, 60), AbsoluteLayoutFlags.PositionProportional);
+
+			Content = abs;
+		}
+	}
+}
+
+
