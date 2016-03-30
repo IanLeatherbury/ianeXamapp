8b935bbb72680dec6b122fe3f278811b7a65c3cd
Ian Leatherbury <ian.leatherbury@gmail.com>
2016-03-29 21:36:12 -0700

    finished step 6

--- ianexamapp/HomePage.cs
+++ ianexamapp/HomePage.cs
@@ -17,12 +17,18 @@ namespace ianexamapp
 
             var abs = new AbsoluteLayout();
             var button = new Button { Text = "Start eXam!", TextColor = Color.Navy, BackgroundColor = Color.Silver };
+            button.Clicked += Button_Clicked;
 
             //add background image first, so it's first in the z order
             abs.Children.Add(backgroundImage, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.SizeProportional);
             abs.Children.Add(button, new Rectangle(0.5, 0.5, 150, 60), AbsoluteLayoutFlags.PositionProportional);           
 
             Content = abs;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new QuestionPage());
         }
     }
 }
