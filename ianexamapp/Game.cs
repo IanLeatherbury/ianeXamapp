2cf4ab203d81c875e5b3b7379d2b84de8e85156c
Ian Leatherbury <ian.leatherbury@gmail.com>
2016-04-02 12:44:23 -0700

    Finished exercise 8

---
+++ ianexamapp/Game.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace ianexamapp
+{
+    public class Game
+    {
+        public List<QuizQuestion> Questions { get; private set; }
+        public bool?[] Responses { get; private set; }
+
+        public QuizQuestion CurrentQuestion => Questions[QuestionIndex];
+        public bool? CurrentResponse => Responses[QuestionIndex];
+        int QuestionIndex { get; set; }
+
+        public int NumberOfQuestions => Questions?.Count ?? 0;
+
+        public Game(List<QuizQuestion> questions)
+        {
+            Questions = questions;
+            Responses = new bool?[Questions.Count];
+        }
+
+        public bool NextQuestion()
+        {
+            if (QuestionIndex < NumberOfQuestions - 1)
+            {
+                QuestionIndex++;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Restart()
+        {
+            QuestionIndex = 0;
+
+            Responses = new bool?[Questions.Count];
+        }
+
+        public void OnTrue()
+        {
+            Responses[QuestionIndex] = true;
+        }
+
+        public void OnFalse()
+        {
+            Responses[QuestionIndex] = false;
+        }
+
+        public int GetNumberOfCorrectResponses()
+        {
+            int count = 0;
+
+            for (int i = 0; i < Responses.Length; i++)
+            {
+                if (IsQuestionCorrect(i) == true)
+                    count++;
+            }
+
+            return count;
+        }
+
+        bool IsQuestionCorrect(int index)
+        {
+            if (index < 0 || index >= NumberOfQuestions)
+                return false;
+
+            if (Responses[index] == null)
+                return false;
+
+            if (Responses[index].Value == Questions[index].Answer)
+                return true;
+
+            return false;
+        }
+    }
+}
--- ianexamapp/ianexamapp.cs
+++ ianexamapp/ianexamapp.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using System.IO;
+using System.Reflection;
+using eXam;
 using Xamarin.Forms;
 
 namespace ianexamapp
 {
 	public class App : Application
 	{
+        public static Game CurrentGame { get; private set; }
 		public App ()
 		{
 			// The root page of your application
@@ -14,7 +17,13 @@ namespace ianexamapp
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
+            // Handle when your app starts
+            var stream = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("ianexamapp.Data.questions.xml");
+            var questionText = new StreamReader(stream).ReadToEnd();
+		    var questions = QuizQuestionXmlSerializer.Deserialize(questionText);
+
+            CurrentGame = new Game(questions);
+
 		}
 
 		protected override void OnSleep ()
